4246809b067d0705e1136a3243ad06f8b9bcc8b7
Samuele Resca <samuele.resca@gmail.com>
2018-03-27 12:53:21 +0200

    refactor

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet restore src/Blog.FSharpWebAPI
+dotnet build src/Blog.FSharpWebAPI
+
---
+++ src/Blog.FSharpWebAPI/DbContext.fs
@@ -0,0 +1,10 @@
+﻿namespace EFCore.DataAccess
+open System
+open Microsoft.EntityFrameworkCore
+open Microsoft.EntityFrameworkCore.Storage.Converters
+
+type SerieContext =
+    inherit DbContext
+    
+    new() = { inherit DbContext() }
+    new(options: DbContextOptions<SerieContext>) = { inherit DbContext(options) }
